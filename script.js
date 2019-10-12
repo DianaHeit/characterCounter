da0c1fae88b0ca7986a7a6204689f5b6e574c82b
DianaHeit <diana.heit@gmx.de>
2019-10-12 12:18:06 +0200

    JS correction

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function counter(str) {
-	var typed = str.length;
-	document.getElementById("charCount").innerHTML = typed + ' characters';
-  var tooMany = typed-3;
+  var typed = str.length;
+  document.getElementById("charCount").innerHTML = typed + ' characters';
+  var tooMany = typed - 3;
   if (typed > 3) {
-  alert("You have written " + typed + " characters - those are " + tooMany + " too many! Delete " + tooMany + ".");
+    alert("You have written " + typed + " characters - those are " + tooMany + " too many! Delete " + tooMany + ".");
+  }
 }
-};
+
+// function under3(str) {
+//   var typed = str.length;
+//   var under3 = typed.slice(0,4);
+// }
